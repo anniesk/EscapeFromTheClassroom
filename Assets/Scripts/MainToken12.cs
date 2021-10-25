90159ebe40d19e32be275d33e7e4c5ccc5de10cb
Jasperi Sivenius <jasperi.m.sivenius@utu.fi>
2021-10-25 18:06:17 +0300

    Puzzle D added

---
+++ Assets/Scripts/MainToken12.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainToken12 : MonoBehaviour
+{
+    GameObject gameControl;
+    SpriteRenderer spriteRenderer;
+    public Sprite face;
+    public Sprite back;
+
+
+    public void OnMouseDown()
+    {
+        // Tarkistaa onko taman verbin kaikki muodot jo loydetty
+        // (be waswere been, jos on, niin ei anna tehda mitaan)
+        if (gameControl.GetComponent<GameControl>().Loydettyget == true) {
+
+        }
+        else {
+            if (gameControl.GetComponent<GameControl>().kuinkamontakaannetty == 3) {
+                gameControl.GetComponent<GameControl>().KaannaEiLoydetyt();
+                gameControl.GetComponent<GameControl>().kuinkamontakaannetty = 0;
+            }
+            if(spriteRenderer.sprite == back)
+            {
+                gameControl.GetComponent<GameControl>().Tila12 = true;
+                gameControl.GetComponent<GameControl>().kuinkamontakaannetty++;
+                spriteRenderer.sprite = face;
+            }
+            else
+            {
+                gameControl.GetComponent<GameControl>().Tila12 = false;
+                gameControl.GetComponent<GameControl>().kuinkamontakaannetty--;
+                spriteRenderer.sprite = back;
+            }
+            gameControl.GetComponent<GameControl>().Tarkista();
+        }
+    }
+
+    private void Awake()
+    {
+        gameControl = GameObject.Find("GameControl");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
