f58c654afb7f6b05dc04c8530b87d84c81222faf
Miisa Andersin <miisa.e.andersin@utu.fi>
2021-12-01 19:23:09 +0200

    ikkuna-animaatio

--- Assets/Scripts/ikkuna_animaatio.cs
+++ Assets/Scripts/ikkuna_animaatio.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 
 public class ikkuna_animaatio : MonoBehaviour
 {
+    public Canvas canvas;
+    public GameObject timer;
     public Animator ikkuna;
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        timer = canvas.transform.Find("Timer").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*float timerAika;
-        if(timerAika > 10000){
-            ikkuna.SetFloat("time", 5);
+        float timerAika = timer.GetComponent<Timer>().returnFloat();
+        
+        /*if (timerAika < 900 && timerAika > 675){
+            ikkuna.SetFloat("time", 2);
         }
-        else if (timerAika > 1000){
-            ikkuna.SetFloat("time", 4);
+        else*/ if (timerAika < 675 && timerAika > 450){
+            ikkuna.SetFloat("time", 2);
         }
-        else if (timerAika > 100){
+        else if (timerAika < 450 && timerAika > 225){
             ikkuna.SetFloat("time", 3);
         }
-        else if (timerAika > 10){
-            ikkuna.SetFloat("time", 2)
-        }*/
+        else if (timerAika <225){
+            ikkuna.SetFloat("time", 4);
+        }
     }
 }
